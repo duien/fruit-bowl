19d7d6ecea6045c1b307a38f0d98663a1330f485
Emily Price <price.emily@gmail.com>
2010-08-09 20:43:14 -0400

    Less busted photo updating

--- lib/photo.rb
+++ lib/photo.rb
@@ -5,6 +5,7 @@ class Photo < Item
   key :source_url
   key :page_url
   key :tags, Array
+  key :photo_id, :unique => true
 
   def self.by_date
     where(:published_at.lt => Time.now).sort(:published_at.desc)
@@ -14,7 +15,7 @@ class Photo < Item
     user = connection.people.find_by_username('duien')
     last_photo = sort(:published_at.desc).first
     search_options = { :user_id => user.nsid }
-    search_options.merge(:min_upload_date => last_photo.published_at) if last_photo
+    search_options.merge!(:min_upload_date => last_photo.published_at.to_i) if last_photo
     photos = connection.photos.search(search_options)
     photos.each do |photo|
       Photo.create(:title => photo.title,
@@ -22,8 +23,10 @@ class Photo < Item
                    :body => photo.description,
                    :tags => photo.tags.split(' '),
                    :page_url => photo.url_photopage,
+                   :photo_id => photo.id,
                    :source_url => "http://farm#{photo.farm}.static.flickr.com/#{photo.server}/#{photo.id}_#{photo.secret}_m.jpg")
     end
+    "Photos updated"
   end
 
   private
