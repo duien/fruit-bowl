891d000f8d1d696fba99e2ad65c87e067ec0d462
Emily Price <price.emily@gmail.com>
2010-08-14 22:45:26 -0400

    Automatically set Post stubs

--- lib/post.rb
+++ lib/post.rb
@@ -4,8 +4,14 @@ class Post < Item
   key :stub, :unique => true
   key :tags, Array
 
+  before_create :set_stub
+
   def self.by_date
     where(:published_at.lt => Time.now).sort(:published_at.desc)
+  end
+
+  def set_stub
+    self.stub = title.parameterize.to_s if stub.blank?
   end
 
 end
