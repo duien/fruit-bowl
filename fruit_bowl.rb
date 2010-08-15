000415f9e2aa8b1543670d1305747542a0329012
Emily Price <price.emily@gmail.com>
2010-08-15 14:02:01 -0400

    Back-end for pagination links

--- fruit_bowl.rb
+++ fruit_bowl.rb
@@ -22,8 +22,9 @@ class FruitBowl < Sinatra::Base
 
   before do
     content_type :html, 'charset' => 'utf-8'
-    params[:per_page] = params[:per_page] ? params[:per_page].to_i : 10
-    params[:page] = params[:page] ? params[:page].to_i : 0
+    params[:per_page] = ( params[:per_page] ? params[:per_page].to_i : 10 )
+    params[:page] = ( params[:page] ? params[:page].to_i : 0 )
+    Log.debug("Params: #{params.inspect}")
   end
   
 
@@ -35,12 +36,14 @@ class FruitBowl < Sinatra::Base
 
   get '/everything/?' do
     @items = Item.by_date.limit(params[:per_page]).skip(params[:page] * params[:per_page])
+    @item_count = Item.by_date.count
     @type = :everything
     haml :index
   end
 
   get '/' do
     @items = Post.by_date.limit(params[:per_page]).skip(params[:page] * params[:per_page])
+    @item_count = Post.by_date.count
     @type = :blog
     haml :index
   end
@@ -58,24 +61,28 @@ class FruitBowl < Sinatra::Base
 
   get '/twitter/?' do
     @items = Tweet.by_date.limit(params[:per_page]).skip(params[:page] * params[:per_page])
+    @item_count = Tweet.by_date.count
     @type = :twitter
     haml :index
   end
 
   get '/delicious/?' do
     @items = Bookmark.by_date.limit(params[:per_page]).skip(params[:page] * params[:per_page])
+    @item_count = Bookmark.by_date.count
     @type = :delicious
     haml :index
   end
 
   get '/flickr/?' do
     @items = Photo.by_date.limit(params[:per_page]).skip(params[:page] * params[:per_page])
+    @item_count = Photo.by_date.count
     @type = :flickr
     haml :index
   end
 
   get '/reader/?' do
     @items = Share.by_date.limit(params[:per_page]).skip(params[:page] * params[:per_page])
+    @item_count = Share.by_date.count
     @type = :reader
     haml :index
   end
@@ -87,25 +94,51 @@ class FruitBowl < Sinatra::Base
 
   private
 
-  def site_link(site)
-    case site
-    when :blog
-      '/'
-    when :twitter
-      'http://twitter.com/duien'
-    when :delicious
-      'http://delicious.com/duien'
-    when :flickr
-      'http://flickr.com/photos/duien'
-    when :reader
-      'http://www.google.com/reader/shared/price.emily'
-    when :everything
-      '/everything'
+  helpers do
+    def site_link(site)
+      case site
+      when :blog
+        '/'
+      when :twitter
+        'http://twitter.com/duien'
+      when :delicious
+        'http://delicious.com/duien'
+      when :flickr
+        'http://flickr.com/photos/duien'
+      when :reader
+        'http://www.google.com/reader/shared/price.emily'
+      when :everything
+        '/everything'
+      end
     end
-  end
 
-  def format_date(date)
-    date ? date.strftime('%b %d, %Y') : "UNPUBLISHED"
+    def prev_page?
+      params[:page] > 0
+    end
+
+    def prev_page
+      base = "/#{@type unless @type == :blog}"
+      page_params = {}
+      page_params.merge!( :page => params[:page] - 1 ) unless params[:page] == 1
+      page_params.merge!( :per_page => params[:per_page] ) unless params[:per_page] == 10
+      "#{base}#{'?' unless page_params.empty?}#{page_params.map{ |k,v| "#{k}=#{v}" }.join('&')}"
+    end
+
+    def next_page?
+      @item_count && @item_count > ( (params[:page]+1) * params[:per_page] )
+    end
+
+    def next_page
+      base = "/#{@type unless @type == :blog}"
+      page_params = { :page => params[:page] + 1 }
+      page_params.merge!( :per_page => params[:per_page] ) unless params[:per_page] == 10
+      "#{base}#{'?' unless page_params.empty?}#{page_params.map{ |k,v| "#{k}=#{v}" }.join('&')}"
+    end
+
+    def format_date(date)
+      date ? date.strftime('%b %d, %Y') : "UNPUBLISHED"
+    end
+
   end
 
 end
